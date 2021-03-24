dacd4c13de14973176b6cb3c526ddd6bbfe7172c
Jadir Junior <jadirjsjunior@gmail.com>
2021-03-24 16:03:23 -0300

    arrumado logica de auto like

--- public/content.js
+++ public/content.js
@@ -1,7 +1,16 @@
 /*global chrome */
+var randomPeriod = 0;
+var unlikeNames = [];
+var unlikeBios = [];
+var settime = null;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.data === "like") {
+  if (request.data.type === "like") {
+    const config = request.data;
+    randomPeriod = config.time;
+    unlikeNames = config.names;
+    unlikeBios = config.bios;
+
     loopLike();
   }
 });
@@ -12,6 +21,55 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 });
 
+const checkTinder = () => {
+  var base = "https://tinder.com/";
+  return (
+    window.location.href.startsWith(base + "app/recs") ||
+    window.location.href.startsWith(base + "app/matches")
+  );
+};
+
+function haveWord(text, words) {
+  if (!text || !words) {
+    return false;
+  }
+
+  const result = words?.filter((word) =>
+    text.toLowerCase().includes(word?.name.toLowerCase())
+  );
+
+  if (result.length > 0) {
+    return true;
+  }
+
+  return false;
+}
+
+const likeOrUnlike = () => {
+  var name = document.getElementsByClassName("Fz($xl) Fw($bold)")[1]?.innerHTML;
+  var description = document.getElementsByClassName("BreakWord")[0]?.innerHTML;
+  var unlikeButton = document.getElementsByClassName("button")[1];
+  var likeButton = document.getElementsByClassName("button")[3];
+
+  if (haveWord(name, unlikeNames) || haveWord(description, unlikeBios)) {
+    console.log("unlike name + description:", `${name} - ${description}`);
+    unlikeButton.click();
+  } else {
+    likeButton.click();
+  }
+};
+
 const loopLike = () => {
-  var randomPeriod = 500;
+  console.log("time:", randomPeriod);
+  console.log("unlike Names", unlikeNames);
+  console.log("unlike bios", unlikeBios);
+
+  settime = setTimeout(() => {
+    if (checkTinder()) {
+      likeOrUnlike();
+      loopLike();
+    } else {
+      clearTimeout(settime);
+    }
+  }, randomPeriod);
 };
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,22 +1,32 @@
+/*global chrome*/
+
 import * as S from "./styles";
 
 import IconButton from "../../components/IconButton/IconButton";
 import { Settings as IconSettings } from "@styled-icons/feather/Settings";
 import { Link } from "react-router-dom";
+import { getBios } from "../../services/bio";
+import { getNames } from "../../services/names";
+import { getTime } from "../../services/time";
 
 const Home = () => {
   const sendMessage = (data, cb) => {
-    // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    //   chrome.tabs.sendMessage(tabs[0].id, { data: data }, () => {
-    //     cb();
-    //   });
-    // });
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { data: data }, () => {
+        cb();
+      });
+    });
   };
 
   const like = () => {
-    sendMessage("like", () => {
-      window.close();
-    });
+    const config = {
+      time: getTime(),
+      names: getNames(),
+      bios: getBios(),
+      type: "like",
+    };
+
+    sendMessage(config, () => {});
   };
 
   return (
@@ -27,7 +37,7 @@ const Home = () => {
         </IconButton>
       </Link>
 
-      {/* <button onClick={like}>Like</button> */}
+      <button onClick={like}>Like</button>
     </S.Wrapper>
   );
 };
